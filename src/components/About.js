86d7a2054c61c6b3bb694dd9bb40a428bd52839a
Devon <dbatemon33@gmail.com>
2022-06-24 08:57:02 -0700

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import Heading from './Heading';
+import Code from '../assets/png/coding.jpg';
 
 export default function About() {
   return (
@@ -13,12 +14,15 @@ export default function About() {
                 className='px-4 py-10 bg-space rounded-xl xs:py-14 xs:px-8'
             >
                 <div className='text-base xs:text-lg text-slate-300 md:text-xl'>
-                    <p className='mb-4 text-left xs:mb-6'>I am a software developer that has a background in digital art. I combine my skills as an artist and my passion for technology to make my ideas come to life. <strong className='font-bold text-hotpink-200'>I am a creator</strong>, I always have been. From playing with legos, painting on canvas, or typing lines of code on an IDE, I love creating and that is my passion.</p>
+                    <p className='mb-4 text-left xs:mb-6'>I am a software developer that has a background in digital art. I combine my skills as an artist and my passion for technology to make my ideas come to life. <strong className='font-bold text-hotpink-200'>I am a creator</strong>.</p>
                     <p className='hidden mb-4 text-left xs:mb-6 md:block'>
-                    Programming allows me to build for the future, and that makes me super excited. I want to be working with cutting-edge technology and building tools that will solve future problems. The most fufilling thing about programming is manifesting your vision and your ideas. Software gives value to people. That's why I am passionate about programming. 
+                    Programming allows me to Showcase my Artistic Capabilities, and that makes me ecstatic at what my future skillset can achieve. I want to be working with cutting-edge technology and building tools that will leave people amazed everytime they visit A site I've worked on or created. The most fufilling thing about programming is manifesting your vision and your ideas. That's why I am passionate about programming. 
                     </p>
-                    <p className='mb-4 text-left xs:mb-6'>When I am not coding, I like to hang out with friends on Discord, go to the gym, and create art.</p>
-                    <p className='px-3 py-2 text-gray-400 border-l-8 border-hotpink-200 bg-denim xs:py-4 xs:px-6'>I wish I could buy me a spaceship and fly past the sky - Kanye West</p>
+                    <p className='mb-4 text-left xs:mb-6'>When I'm not coding, I like to play competitive games online, Collect cards such as Pokemon and NBA Prizm, and create art with Photoshop and Clip studio paint.</p>
+                    <div className='px-3 py-2 text-gray-400 border-l-8 border-hotpink-200 bg-denim xs:py-4 xs:px-6'>
+                        {/* <img className='rounded-x4 h-64 mb-5' src={Code} alt="" />
+                        <img className='rounded-x4 h-64 mb-5' src={Code} alt="" /> */}
+                    </div>
                 </div>
             </div>
         </div>
