0f79cb12f091acd9c7af6c2f22f0b1df52e88d89
Devon <dbatemon33@gmail.com>
2022-07-10 22:23:33 -0700

    update again

--- src/components/About.js
+++ src/components/About.js
@@ -16,9 +16,9 @@ export default function About() {
                 <div className='text-base xs:text-lg text-slate-300 md:text-xl'>
                     <p className='mb-4 text-left xs:mb-6'>I am a software developer that has a background in digital art. I combine my skills as an artist and my passion for technology to make my ideas come to life. <strong className='font-bold text-hotpink-200'>I am a creator</strong>.</p>
                     <p className='hidden mb-4 text-left xs:mb-6 md:block'>
-                    Programming allows me to showcase my artistic capabilities, and that makes me ecstatic at what my ever-growing skillset can achieve. I want to be working with cutting-edge technology and building tools that will leave people amazed everytime they visit A site I've worked on or created. The most fufilling thing about programming is manifesting your vision and your ideas. That's why I am passionate about programming. 
+                    Programming allows me to showcase my artistic capabilities, and that makes me ecstatic at what my ever-growing skillset can achieve. I want to be working with cutting-edge technology and building tools that will leave people amazed everytime they visit a site I've worked on or created. The most fufilling thing about programming is manifesting your vision and your ideas. That's why I am passionate about programming. 
                     </p>
-                    <p className='mb-4 text-left xs:mb-6'>Outside of coding I like to play competitive games online, Collect cards such as Pokemon and NBA Prizm, and create art with Photoshop and Clip Studio Paint.</p>
+                    <p className='mb-4 text-left xs:mb-6'>Outside of coding I like to play competitive online games, Collect cards such as Pokemon and NBA Prizm, and create art with Photoshop and Clip Studio Paint.</p>
                     <div className='px-3 py-2 text-gray-400 border-l-8 border-hotpink-200 bg-denim xs:py-4 xs:px-6'>
                         {/* <img className='rounded-x4 h-64 mb-5' src={Code} alt="" />
                         <img className='rounded-x4 h-64 mb-5' src={Code} alt="" /> */}
